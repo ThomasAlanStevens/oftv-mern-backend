96f4e1a5efe6b2e4fb11e0aa877350454aac1c33
thomas <thomaalanstevens@gmail.com>
2022-08-14 10:52:01 -0700

    Added get operation.

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
 // CRUD Operations
 
 // Get Operations
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
